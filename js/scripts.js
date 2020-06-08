1f1349daefe7a10a0b8d63930692a893ed090614
Ester <ester.tataje@gmail.com>
2020-06-08 15:59:25 -0700

    add README and start of new webpage with arrays using split method
    co-authored-by: Deryck Jackson <Dgjackson01@gmail.com>
    co-athhored-by: Ester Tataje <ester.tataje@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,18 @@
-$(document).ready(function(){}
+$(document).ready(function() {
+  $("form").submit(function(event) {
+    event.preventDefault();
+    const sentence = $("input#sentence").val().split(" ");
+    let reversedSentence = [];
+    
+    sentence.forEach(function(word) {
+      if (word.length >= 3) {
+        
+      }
+    });
+
+
+
+
+
+  });
+});
